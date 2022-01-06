e0d7df125a5580aa93a058b63600ad0a93852b98
AnyaWMa <wanjingma1122@gmail.com>
2022-01-06 14:44:20 -0800

    update page break

---
+++ src/game_break.js
@@ -0,0 +1,252 @@
+/* mid block page */
+var mid_block_page_1 = {
+    type: "html-keyboard-response",
+    stimulus: `
+   <div>
+    <h1>Good work, young wizard!</h1>
+   <div>
+       <p class="center" style="position: relative; top: 50%; ">You are halfway through the valley, and you decide to camp near a small village.
+      <br>
+      <br>
+      In the village, you meet another adventurer who joins your journey! Here are your gold coins so far:
+      </p>
+      
+   </div>
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="scene" src="assets/half_valley.png" alt="backgroun image with hills and trees">
+    <img class = 'adventure_mid_break' src="assets/adventurer1.gif" alt="adventure with harp">
+</div>
+   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
+      `,
+    choices: [' '],
+    on_start: function() {
+        //set progress bar to 0 at the start of experiment
+        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
+    }
+}
+//<p class = 'coin_counting'>200</p>
+var mid_block_page_2 = {
+    type: "html-keyboard-response",
+    stimulus: `
+   <div>
+    <h1>Good work, young wizard!</h1>
+   <div>
+       <p class="center" style="position: relative; top: 50%; ">You are halfway through the valley, and you decide to camp near a small village.
+      <br>
+      <br>
+      In the village, you meet another adventurer who joins your journey!</p>
+   </div>
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+     <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+     <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="scene" src="assets/half_valley.png" alt="backgroun image with hills and trees">
+    <img class = 'adventure_mid_break' src="assets/adventurer1.gif" alt="adventure with harp">
+    <img class = 'adventure_mid_break' src="assets/adventurer3.gif" alt="adventure with making heart shapes">
+</div>
+   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
+    
+      `,
+    choices: [' '],
+    on_start: function() {
+        //set progress bar to 0 at the start of experiment
+        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
+    }
+}
+
+var mid_block_page_3 = {
+    type: "html-keyboard-response",
+    stimulus: `
+   <div>
+    <h1>Good work, young wizard!</h1>
+   <div>
+       <p class="center" style="position: relative; top: 50%; ">You are halfway through the valley, and you decide to camp near a small village.
+      <br>
+      <br>
+      In the village, you meet another adventurer who joins your journey!</p>
+   </div>
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+     <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+     <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+      <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+      <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+   <img class="scene" src="assets/half_valley.png" alt="backgroun image with hills and trees">
+   <img class = 'adventure_mid_break'  src="assets/adventurer1.gif" alt="adventure with harp">
+   <img class = 'adventure_mid_break'  src="assets/adventurer3.gif" alt="adventure playing rainbow">
+   <img class = 'adventure_mid_break'  src="assets/adventurer2.gif" alt="adventure making heart shapes">\t
+</div>
+   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
+    
+      `,
+    choices: [' '],
+    on_start: function() {
+        //set progress bar to 0 at the start of experiment
+        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
+    }
+}
+
+/* post block page */
+
+var post_block_page_1 = {
+    type: "html-keyboard-response",
+    stimulus: `
+   <div>
+    <h1>Congratulations, young wizard!</h1>
+  <div>
+   <p class="center">You found the guardian and made it through the valley. 
+   <br>
+   <br> 
+   You&#39re getting closer to the gate! Here are your gold coins so far:</p>
+   </div>
+   <div>
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+   <img class="scene" src="assets/valley.png" alt="background image of hills and trees">
+\t\t<img class = 'wizard' src="assets/wizard_coin.gif" alt="adventure playing rainbow">
+\t\t<img class=" guardian" src="assets/guardian1.gif" alt="adventure making heart shapes">
+\t</div>
+
+</div>
+   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
+      `,
+    choices: [' '],
+    on_start: function() {
+        //set progress bar to 0 at the start of experiment
+        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
+    }
+}
+
+var post_block_page_2 = {
+    type: "html-keyboard-response",
+    stimulus: `
+   <div>
+    <h1>Congratulations, young wizard!</h1>
+  <div>
+   <p class="center">You found the guardian and made it through the valley. 
+   <br>
+   <br> 
+   You&#39re getting closer to the gate! Here are your gold coins so far:</p>
+   </div>
+   <div>
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+    <img class="coin" src="assets/coinicon.png" alt="coin">
+   <img class="scene" src="assets/valley.png" alt="background image of hills and trees">
+\t\t<img class = 'wizard' src="assets/wizard_coin.gif" alt="adventure playing rainbow">
+\t\t<img class = 'guardian' src="assets/guardian2.gif" alt="adventure making heart shapes">
+\t</div>
+
+</div>
+   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
+      `,
+    choices: [' '],
+    on_start: function() {
+        //set progress bar to 0 at the start of experiment
+        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
+    }
+}
+
+
+var mid_block_page_list = [mid_block_page_1,mid_block_page_2,mid_block_page_3]
+
+var post_block_page_list = [post_block_page_1,post_block_page_2]
+
+var final_page_1 = {
+    type: "html-keyboard-response",
+    stimulus: `
+   <div>
+    <h1>Finally, you summon the last guardian!</h1>
+   <div>
+   <p class="center"> It agrees to help you open the gate to return home.
+   <br>
+   <br>
+   You say farewell to your new friends and leave the land of Lexicality. Until next time!
+  </p>
+   </div>
+   <div>
+   <img class="scene" src="assets/ending.png" alt="background image of gate">
+   <img class = 'guardian' src="assets/guardian3.gif" alt="image of a unicorn winking">
+   </div>
+
+</div>
+   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
+      `,
+    choices: [' '],
+    on_start: function() {
+        //set progress bar to 0 at the start of experiment
+        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
+    }
+}
+
+var final_page_2 = {
+    type: "html-keyboard-response",
+    stimulus: `
+   <div>
+    <h1>Finally, you summon the last guardian!</h1>
+   <div>
+   <p class="center"> It agrees to help you open the gate to return home.
+   <br>
+   <br>
+   You say farewell to your new friends and leave the land of Lexicality. Until next time!
+  </p>
+   </div>
+   <div>
+   <img class="scene" src="assets/ending.png" alt="background image of gate">
+   <img  class = 'guardian' src="assets/guardian3.gif" alt="image of a unicorn winking">
+   </div>
+
+</div>
+   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
+      `,
+    choices: [' '],
+    on_start: function() {
+        //set progress bar to 0 at the start of experiment
+        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
+    }
+}
+
+
+var final_page_3 = {
+    type: "html-keyboard-response",
+    stimulus: `
+   <div>
+    <h1>Finally, you summon the last guardian!</h1>
+   <div>
+   <p class="center"> It agrees to help you open the gate to return home.
+   <br>
+   <br>
+   You say farewell to your new friends and leave the land of Lexicality. Until next time!
+  </p>
+   </div>
+   <div>
+   <img class="scene" src="assets/ending.png" alt="background image of gate">
+
+   <img class = 'guardian' src="assets/guardian3.gif" alt="image of a unicorn winking">
+   </div>
+
+</div>
+   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
+      `,
+    choices: [' '],
+    on_start: function() {
+        //set progress bar to 0 at the start of experiment
+        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
+    }
+}
+
+var final_page_list = [final_page_1,final_page_2,final_page_3]
--- src/introduction.js
+++ src/introduction.js
@@ -37,7 +37,6 @@ var intro_2 = {
        <p> The real words will be ones you recognize. They are real English words like is, and, basket, or lion. <span class="blue"><b> If you see a real word, press the RIGHT ARROW KEY.</b></span></p>
      </div>
     </div>
-
     <div class="row">
      <div class="column_2_lower" style="background-color:#f2f2f2;">
      <img width="100%" src="assets/arrow_left_p2.png" alt="Magic Word, Press the Left Arrow Key" align="right">
@@ -46,7 +45,6 @@ var intro_2 = {
      <img width="100%" src="assets/arrow_right_p2.png" alt="Real Word, Press the Right Arrow key">
      </div>
     </div>
-
     <div class="button">Press <span class="yellow">Space</span> to continue
     </div>
       `,
@@ -78,9 +76,9 @@ var intro_3 = {
 };
 
 var post_practice_intro = {
-  type: "audio-keyboard-response",
-  stimulus: '../audio/page4.wav',
-  prompt: `
+    type: "audio-keyboard-response",
+    stimulus: '../audio/page4.wav',
+    prompt: `
     <h1>Great work, you are ready to begin the journey! </h1>
       <div>
         <p class="center"> As you travel through the valley, you&#39ll earn gold coins to bring home.</p>
@@ -90,7 +88,7 @@ var post_practice_intro = {
     <div class="button">Press <span class="yellow">Space</span> to begin the game</div>`,
     choices: [' '],
     on_start: function() {
-      jsPsych.setProgressBar(0);
+        jsPsych.setProgressBar(0);
     }
 };
 
@@ -200,288 +198,3 @@ var countdown_trial_1= {
         jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
     }
 };
-
-var mid_block_page_1 = {
-    type: "html-keyboard-response",
-    stimulus: `
-   <div>
-    <h1>Good work, young wizard!</h1>
-   <div>
-       <p class="center" style="position: relative; top: 50%; ">You are halfway through the valley, and you decide to camp near a small village.
-      <br>
-      <br>
-      In the village, you meet another adventurer who joins your journey!</p>
-   </div>
-   <img class="scene" src="assets/half_valley.png" alt="backgroun image with hills and trees">
-   <img style="position: relative; margin-bottom: -10%; bottom: 100px" height="220px" src="assets/adventurer1.gif" alt="adventure with harp">
-</div>
-   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
-    
-      `,
-    choices: [' '],
-    on_start: function() {
-        //set progress bar to 0 at the start of experiment
-        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
-    }
-}
-
-var mid_block_page_2 = {
-    type: "html-keyboard-response",
-    stimulus: `
-   <div>
-    <h1>Good work, young wizard!</h1>
-   <div>
-       <p class="center" style="position: relative; top: 50%; ">You are halfway through the valley, and you decide to camp near a small village.
-      <br>
-      <br>
-      In the village, you meet another adventurer who joins your journey!</p>
-   </div>
-   <img class="scene" src="assets/half_valley.png" alt="backgroun image with hills and trees">
-   <img style="position: relative; margin-bottom: -10%; bottom: 100px" height="220px" src="assets/adventurer1.gif" alt="adventure with harp">
-   <img style="position: relative; margin-bottom: -10%; bottom: 100px" height="220px" src="assets/adventurer3.gif" alt="adventure playing rainbow">
-</div>
-   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
-    
-      `,
-    choices: [' '],
-    on_start: function() {
-        //set progress bar to 0 at the start of experiment
-        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
-    }
-}
-
-var mid_block_page_3 = {
-    type: "html-keyboard-response",
-    stimulus: `
-   <div>
-    <h1>Good work, young wizard!</h1>
-   <div>
-       <p class="center" style="position: relative; top: 50%; ">You are halfway through the valley, and you decide to camp near a small village.
-      <br>
-      <br>
-      In the village, you meet another adventurer who joins your journey!</p>
-   </div>
-   <img class="scene" src="assets/half_valley.png" alt="backgroun image with hills and trees">
-   <img style="position: relative; margin-bottom: -10%; bottom: 100px" height="220px" src="assets/adventurer1.gif" alt="adventure with harp">
-   <img style="position: relative; margin-bottom: -10%; bottom: 100px" height="220px" src="assets/adventurer3.gif" alt="adventure playing rainbow">
-   <img style="position: relative; margin-bottom: -10%; bottom: 100px" height="220px" src="assets/adventurer2.gif" alt="adventure making heart shapes">\t
-</div>
-   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
-    
-      `,
-    choices: [' '],
-    on_start: function() {
-        //set progress bar to 0 at the start of experiment
-        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
-    }
-}
-
-
-var mid_block_page = {
-    type: "html-keyboard-response",
-    stimulus: `
-   <div>
-    <h1>Good work, young wizard!</h1>
-   <div>
-       <p class="center" style="position: relative; top: 50%; ">You are halfway through the valley, and you decide to camp near a small village.
-      <br>
-      <br>
-      In the village, you meet another adventurer who joins your journey!</p>
-   </div>
-   <img class="scene" src="assets/half_valley.png" alt="backgroun image with hills and trees">
-   <img style="position: relative; margin-bottom: -10%; bottom: 100px" height="220px" src="assets/adventurer1.gif" alt="adventure with harp">
-   <img style="position: relative; margin-bottom: -10%; bottom: 100px" height="220px" src="assets/adventurer3.gif" alt="adventure playing rainbow">
-   <img style="position: relative; margin-bottom: -10%; bottom: 100px" height="220px" src="assets/adventurer2.gif" alt="adventure making heart shapes">\t
-</div>
-   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
-    
-      `,
-    choices: [' '],
-    on_start: function() {
-        //set progress bar to 0 at the start of experiment
-        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
-    }
-}
-
-var post_block_page_1 = {
-    type: "html-keyboard-response",
-    stimulus: `
-   <div>
-    <h1>Congratulations, young wizard!</h1>
-  <div>
-   <p class="center">You found the guardian and made it through the valley. 
-   <br>
-   <br> 
-   You&#39re getting closer to the gate! Here are your gold coins so far:</p>
-   </div>
-   <div>
-   <img class="scene" src="assets/valley.png" alt="background image of hills and trees">
-\t\t<img style="position: relative; margin-bottom: -10%;bottom: 180px; right: 20%;" height="300px" src="assets/wizard_coin.gif" alt="adventure playing rainbow">
-\t\t<img style="position: relative; margin-bottom: -10%; bottom: 180px" height="220px" src="assets/adventurer1.gif" alt="adventure with harp">
-\t\t<img style="position: relative; margin-left: 10%; margin-bottom: -10%;bottom: 180px; left: 10%;" height="220px" src="assets/guardian1.gif" alt="adventure making heart shapes">
-\t</div>
-
-</div>
-   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
-      `,
-    choices: [' '],
-    on_start: function() {
-        //set progress bar to 0 at the start of experiment
-        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
-    }
-}
-
-var post_block_page_2 = {
-    type: "html-keyboard-response",
-    stimulus: `
-   <div>
-    <h1>Congratulations, young wizard!</h1>
-  <div>
-   <p class="center">You found the guardian and made it through the valley. 
-   <br>
-   <br> 
-   You&#39re getting closer to the gate! Here are your gold coins so far:</p>
-   </div>
-   <div>
-   <img class="scene" src="assets/valley.png" alt="background image of hills and trees">
-\t\t<img style="position: relative; margin-bottom: -10%;bottom: 180px; right: 20%;" height="300px" src="assets/wizard_coin.gif" alt="adventure playing rainbow">
-\t\t<img style="position: relative; margin-bottom: -10%; bottom: 180px" height="220px" src="assets/adventurer1.gif" alt="adventure with harp">
-\t\t<img style="position: relative; margin-bottom: -10%; bottom: 180px" height="220px" src="assets/adventurer3.gif" alt="adventure with harp">
-\t\t<img style="position: relative; margin-left: 10%; margin-bottom: -10%;bottom: 180px; left: 10%;" height="220px" src="assets/guardian2.gif" alt="adventure making heart shapes">
-\t</div>
-
-</div>
-   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
-      `,
-    choices: [' '],
-    on_start: function() {
-        //set progress bar to 0 at the start of experiment
-        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
-    }
-}
-
-
-var mid_block_page_list = [mid_block_page_1,mid_block_page_2,mid_block_page_3]
-
-var post_block_page_list = [post_block_page_1,post_block_page_2]
-
-
-var final_page = {
-    type: "html-keyboard-response",
-    stimulus: `
-   <div>
-    <h1>Finally, you summon the last guardian!</h1>
-   <div>
-   <p class="center"> It agrees to help you open the gate to return home.
-   <br>
-   <br>
-   You say farewell to your new friends and leave the land of Lexicality. Until next time!
-  </p>
-   </div>
-   <div>
-   <img class="scene" src="assets/ending.png" alt="background image of gate">
-   <img style="position: relative; margin-bottom: -10%; bottom: 180px" height="220px" src="assets/adventurer1.gif" alt="adventure with harp">
-   <img style="position: relative; margin-bottom: -10%; bottom: 180px" height="220px" src="assets/adventurer3.gif" alt="adventure with harp">
-   <img style="position: relative; margin-bottom: -10%; bottom: 180px" height="220px" src="assets/adventurer2.gif" alt="adventure with harp">
-   <img style="position: relative; margin-left: 10%; margin-bottom: -10%;bottom: 180px; " height="220px" src="assets/guardian3.gif" alt="image of a unicorn winking">
-   </div>
-
-</div>
-   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
-      `,
-    choices: [' '],
-    on_start: function() {
-        //set progress bar to 0 at the start of experiment
-        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
-    }
-}
-
-var final_page_1 = {
-    type: "html-keyboard-response",
-    stimulus: `
-   <div>
-    <h1>Finally, you summon the last guardian!</h1>
-   <div>
-   <p class="center"> It agrees to help you open the gate to return home.
-   <br>
-   <br>
-   You say farewell to your new friends and leave the land of Lexicality. Until next time!
-  </p>
-   </div>
-   <div>
-   <img class="scene" src="assets/ending.png" alt="background image of gate">
-   <img style="position: relative; margin-bottom: -10%; bottom: 180px" height="220px" src="assets/adventurer1.gif" alt="adventure with harp">
-   <img style="position: relative; margin-left: 10%; margin-bottom: -10%;bottom: 180px; " height="220px" src="assets/guardian3.gif" alt="image of a unicorn winking">
-   </div>
-
-</div>
-   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
-      `,
-    choices: [' '],
-    on_start: function() {
-        //set progress bar to 0 at the start of experiment
-        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
-    }
-}
-
-var final_page_2 = {
-    type: "html-keyboard-response",
-    stimulus: `
-   <div>
-    <h1>Finally, you summon the last guardian!</h1>
-   <div>
-   <p class="center"> It agrees to help you open the gate to return home.
-   <br>
-   <br>
-   You say farewell to your new friends and leave the land of Lexicality. Until next time!
-  </p>
-   </div>
-   <div>
-   <img class="scene" src="assets/ending.png" alt="background image of gate">
-   <img style="position: relative; margin-bottom: -10%; bottom: 180px" height="220px" src="assets/adventurer1.gif" alt="adventure with harp">
-   <img style="position: relative; margin-bottom: -10%; bottom: 180px" height="220px" src="assets/adventurer3.gif" alt="adventure with harp">
-   <img style="position: relative; margin-left: 10%; margin-bottom: -10%;bottom: 180px; " height="220px" src="assets/guardian3.gif" alt="image of a unicorn winking">
-   </div>
-
-</div>
-   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
-      `,
-    choices: [' '],
-    on_start: function() {
-        //set progress bar to 0 at the start of experiment
-        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
-    }
-}
-
-
-var final_page_3 = {
-    type: "html-keyboard-response",
-    stimulus: `
-   <div>
-    <h1>Finally, you summon the last guardian!</h1>
-   <div>
-   <p class="center"> It agrees to help you open the gate to return home.
-   <br>
-   <br>
-   You say farewell to your new friends and leave the land of Lexicality. Until next time!
-  </p>
-   </div>
-   <div>
-   <img class="scene" src="assets/ending.png" alt="background image of gate">
-   <img style="position: relative; margin-bottom: -10%; bottom: 180px" height="220px" src="assets/adventurer1.gif" alt="adventure with harp">
-   <img style="position: relative; margin-bottom: -10%; bottom: 180px" height="220px" src="assets/adventurer3.gif" alt="adventure with harp">
-   <img style="position: relative; margin-bottom: -10%; bottom: 180px" height="220px" src="assets/adventurer2.gif" alt="adventure with harp">
-   <img style="position: relative; margin-left: 10%; margin-bottom: -10%;bottom: 180px; " height="220px" src="assets/guardian3.gif" alt="image of a unicorn winking">
-   </div>
-
-</div>
-   <div class="button">Press <span class="yellow">Space</span> when you&#39re ready to continue</div>
-      `,
-    choices: [' '],
-    on_start: function() {
-        //set progress bar to 0 at the start of experiment
-        jsPsych.setProgressBar((roarTrialNum-1) /(arrSum(stimulusCountLis)));
-    }
-}
-
-var final_page_list = [final_page_1,final_page_2,final_page_3]

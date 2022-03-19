f1dcda4397b18cf5e3ffbbce99f7328b10cbfe4d
Adam Richie-Halford <richiehalford@gmail.com>
2022-03-19 14:01:33 -0700

    Export only what is needed from src/audio.js

--- src/audio.js
+++ src/audio.js
@@ -1,42 +1,35 @@
-/* response feedback: coin_sound vs. fail_sound */
-var audio_response_correct = {
-    type: 'audio-keyboard-response',
-    stimulus: 'audio/coin_sound.wav',
-    choices: jsPsych.NO_KEYS,
-    trial_ends_after_audio: true,
-    prompt:  `<div class = stimulus_div><p class = 'stimulus' style="font-size:60px;"><br></p></div> 
-              <img class="lower" src="assets/arrowkey_lex.png" alt="arrow keys" style=" width:698px; height:120px">`
+import { jsPsych } from "jspsych";
+
+const audio_response_correct = {
+  type: "audio-keyboard-response",
+  stimulus: "audio/coin_sound.wav",
+  choices: jsPsych.NO_KEYS,
+  trial_ends_after_audio: true,
+  prompt: `<div class = stimulus_div><p class = 'stimulus' style="font-size:60px;"><br></p></div> 
+              <img class="lower" src="assets/arrowkey_lex.png" alt="arrow keys" style=" width:698px; height:120px">`,
 };
 
-var if_audio_response_correct = {
-    timeline: [audio_response_correct],
-    conditional_function: function(){
-        if (currentTrialCorrect) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-}
-
-var audio_response_wrong = {
-    type: 'audio-keyboard-response',
-    stimulus: 'audio/fail_sound.wav',
-    choices: jsPsych.NO_KEYS,
-    trial_ends_after_audio: true,
-    prompt:  `<div class = stimulus_div><p class = 'stimulus' style="font-size:60px;"><br></p></div> 
-              <img class="lower" src="assets/arrowkey_lex.png" alt="arrow keys" style=" width:698px; height:120px">`
+const if_audio_response_correct = {
+  timeline: [audio_response_correct],
+  conditional_function: function () {
+    return currentTrial == true;
+  },
 };
 
-var if_audio_response_wrong = {
-    timeline: [audio_response_wrong],
-    conditional_function: function(){
-        if (currentTrialCorrect) {
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-}
+const audio_response_wrong = {
+  type: "audio-keyboard-response",
+  stimulus: "audio/fail_sound.wav",
+  choices: jsPsych.NO_KEYS,
+  trial_ends_after_audio: true,
+  prompt: `<div class = stimulus_div><p class = 'stimulus' style="font-size:60px;"><br></p></div> 
+              <img class="lower" src="assets/arrowkey_lex.png" alt="arrow keys" style=" width:698px; height:120px">`,
+};
+
+const if_audio_response_wrong = {
+  timeline: [audio_response_wrong],
+  conditional_function: function () {
+    return currentTrial == false;
+  },
+};
+
+export { if_audio_response_correct, if_audio_response_wrong };

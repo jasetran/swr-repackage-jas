a25481450a359b06966892655dee111c8405e1b6
Philip Ziolkowski <philipz4848@gmail.com>
2023-04-25 12:07:04 -0700

    Changing countdown and fixation to be same size as buttons on mobile

--- src/experiment.js
+++ src/experiment.js
@@ -485,9 +485,28 @@ const getStimulus = () => {
 const setup_fixation = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus: function () {
-    return `<div class = stimulus_div><p class = 'stimulus'>+</p></div>`;
+    return `<div class='stimulus_div'>
+              <p class='stimulus'>+</p>
+            </div>`;
   },
-  prompt: `<div><img class="lower" src="${imgContent.arrowkeyLex}" alt="arrow keys"></div>`,
+  prompt: () => {
+    if (isTouchScreen) {
+      return (
+        `<div id='${isTouchScreen ? 'countdown-wrapper' : ''}'>
+            <div id='countdown-arrows-wrapper'>
+              <div class="countdown-arrows">
+                <img class='btn-arrows' src=${imgContent.staticLeftKey} alt='left arrow' />
+              </div>
+              <div class="countdown-arrows">
+                <img class='btn-arrows' src=${imgContent.staticRightKey} alt='right arrow' />
+              </div>
+            </div>
+         </div>`
+      )
+    }
+
+    return `<img class="lower" src="${imgContent.arrowkeyLex}" alt = "arrow-key">`
+  },
   choices: "NO_KEYS",
   trial_duration: config.timing.fixationTime,
   data: {
